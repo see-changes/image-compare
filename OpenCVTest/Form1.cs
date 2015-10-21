f557f60d01635599c0e35a1f2a75b696b35ce6a2
ahjota <the.tsunami@gmail.com>
2015-10-21 11:16:49 -0700

    committing chaun's starter project and dan's images

---
+++ OpenCVTest/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace OpenCVTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Mat src = new Mat(@"Images\IMG_20151020_155508729.jpg", ImreadModes.GrayScale);
+            Mat dst = new Mat();
+
+            Cv2.Canny(src, dst, 50, 200);
+            using (new Window("src image", src))
+            using (new Window("dst image", dst))
+            {
+                Cv2.WaitKey();
+            }
+        }
+
+
+    }
+}
