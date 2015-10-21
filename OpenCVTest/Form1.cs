aa8b84ff6c66e4c8987f7ee973683b2519f0c690
Dan Catalano <dev@nwbt.co>
2015-10-21 12:04:04 -0700

    gitignore & form

--- OpenCVTest/Form1.cs
+++ OpenCVTest/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,13 +25,31 @@ namespace OpenCVTest
             Mat dst = new Mat();
 
             Cv2.Canny(src, dst, 50, 200);
-            using (new Window("src image", src))
-            using (new Window("dst image", dst))
+
+            using (new Window("src image", src)) // grey scale representation original method
+            {
+                Cv2.WaitKey();
+            }
+
+            using (new Window("dst image", dst)) // matrix representation of Canny method
             {
                 Cv2.WaitKey();
             }
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var src1 = new Mat(@"C:\workspace\github\image-compare\OpenCVTest\Images\IMG_20151020_155510557.jpg");
+            var src2 = new Mat(@"C:\workspace\github\image-compare\OpenCVTest\Images\IMG_20151020_155508729.jpg");
+            var output = new Mat();
+
+            Cv2.Absdiff(src1, src2, output);
+
+            using (new Window("output of abs diff", WindowMode.KeepRatio, output))
+            {
+                Cv2.WaitKey();
+            }
+        }
 
     }
 }
