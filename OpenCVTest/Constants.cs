d311f03757d7b2c094fb3152b259ff36428964b4
ahjota <the.tsunami@gmail.com>
2015-10-21 16:17:55 -0700

    fixed Constants filename strings

--- OpenCVTest/Constants.cs
+++ OpenCVTest/Constants.cs
@@ -2,10 +2,10 @@
 {
     public class Constants
     {
-        public const string ImageControl1 = @"Images\IM_20151020_155508729.jpg";
-        public const string ImageControl2 = @"Images\IM_20151020_155510557.jpg";
-        public const string ImageTongsMoved = @"Images\IM_20151020_155516713.jpg";
-        public const string ImageTongsGone = @"Images\IM_20151020_155520579.jpg";
-        public const string ImageSpoonsMoved = @"Images\IM_20151020_155534249.jpg";
+        public const string ImageControl1 = @"Images\IMG_20151020_155508729.jpg";
+        public const string ImageControl2 = @"Images\IMG_20151020_155510557.jpg";
+        public const string ImageTongsMoved = @"Images\IMG_20151020_155516713.jpg";
+        public const string ImageTongsGone = @"Images\IMG_20151020_155520579.jpg";
+        public const string ImageSpoonsMoved = @"Images\IMG_20151020_155534249.jpg";
     }
 }
