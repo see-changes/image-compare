6b2221241fb66a16b431785aa68512c34f6b7d72
ahjota <the.tsunami@gmail.com>
2015-10-21 14:52:28 -0700

    added ORB implementation of Find Features

--- OpenCVTest/Form1a.cs
+++ OpenCVTest/Form1a.cs
@@ -1,51 +1,82 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
 using OpenCvSharp;
+
 namespace OpenCVTest
 {
     public partial class Form1 : Form
     {
         /// <summary>
-        /// Keypoint detection.
+        ///     Keypoint detection.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void button3_Click(object sender, EventArgs e)
         {
-            using (var src = new Mat(@"Images\IMG_20151020_155508729.jpg", ImreadModes.GrayScale))
-            using (var dst = new Mat(@"Images\IMG_20151020_155508729.jpg", ImreadModes.GrayScale))
+            var filename = string.Empty;
+
+            filename = @"Images\IMG_20151020_155508729.jpg";
+            DrawFeatures(filename, FindFeatures(filename));
+
+            filename = @"Images\IMG_20151020_155510557.jpg";
+            DrawFeatures(filename, FindFeatures(filename));
+        }
+
+        private IEnumerable<KeyPoint> FindFeatures(string filename)
+        {
+            return FindFeatures_FREAK(filename);
+            //return FindFeatures_HarrisCornerDetector(filename);
+        }
+
+        private IEnumerable<KeyPoint> FindFeatures_FREAK(string filename)
+        {
+            using (var gray = new Mat(filename, ImreadModes.GrayScale))
             {
-                // find íntersting corners
-                var corners = src.GoodFeaturesToTrack(100, 0.01, 100, null, 15, true, 1);
+                // ORB
+                var orb = ORB.Create(1000);
+                KeyPoint[] keypoints = orb.Detect(gray);
+                return keypoints;
+            }
+        }
 
-                var keypoints = corners.Select(s => new KeyPoint(s, 100));
+        private IEnumerable<KeyPoint> FindFeatures_HarrisCornerDetector(string filename)
+        {
+            using (var src = new Mat(filename, ImreadModes.GrayScale))
+            {
+                // find ínteresting corners
+                Point2f[] corners = src.GoodFeaturesToTrack(100, 0.01, 100, null, 15, true, 1);
 
-                if (keypoints != null)
+                IEnumerable<KeyPoint> keypoints = corners.Select(s => new KeyPoint(s, 100));
+                return keypoints;
+            }
+        }
+
+        private void DrawFeatures(string filename, IEnumerable<KeyPoint> keypoints)
+        {
+            using (var dst = new Mat(filename, ImreadModes.GrayScale))
+            {
+                int thickness = 2;
+                foreach (KeyPoint kp in keypoints)
                 {
-                    int thickness = 5;
-                    foreach (var kp in keypoints)
-                    {
-                        var r = kp.Size/2;
-                        Cv2.Circle(dst, kp.Pt, (int)r, Scalar.Red, thickness);
-                        Cv2.Line(dst,
-                            new Point2f(kp.Pt.X + r, kp.Pt.Y + r),
-                            new Point2f(kp.Pt.X - r, kp.Pt.Y - r),
-                            Scalar.Red, thickness);
-                        Cv2.Line(dst,
-                            new Point2f(kp.Pt.X - r, kp.Pt.Y - r),
-                            new Point2f(kp.Pt.X + r, kp.Pt.Y + r),
-                            Scalar.Red, thickness);
-                    }
+                    float r = kp.Size/2;
+                    Cv2.Circle(dst, kp.Pt, (int) r, Scalar.Red, thickness);
+                    Cv2.Line(dst,
+                        new Point2f(kp.Pt.X + r, kp.Pt.Y + r),
+                        new Point2f(kp.Pt.X - r, kp.Pt.Y - r),
+                        Scalar.Red, thickness);
+                    Cv2.Line(dst,
+                        new Point2f(kp.Pt.X - r, kp.Pt.Y - r),
+                        new Point2f(kp.Pt.X + r, kp.Pt.Y + r),
+                        Scalar.Red, thickness);
                 }
-
-                //Cv2.DrawKeypoints(src, keypoints, dst, Scalar.Red);
 
                 using (new Window("src w. keypoints", WindowMode.FreeRatio, dst))
                 {
                     Cv2.WaitKey();
                 }
-        }
+            }
         }
     }
 }
