3a47c3a87cd98560ec58ad221c82d70a71fa6582
ahjota <the.tsunami@gmail.com>
2015-10-21 12:09:45 -0700

    added new partial Form1 for parallel dev

---
+++ OpenCVTest/Form1a.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OpenCVTest
+{
+    public partial class Form1 : Form
+    {
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(@"HI MOM");
+        }
+    }
+}
