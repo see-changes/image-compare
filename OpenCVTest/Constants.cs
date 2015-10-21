240fdd34680091fbe9f9372049ded0465ebe561b
Dan Catalano <dev@nwbt.co>
2015-10-21 16:30:52 -0700

    added to constants

--- OpenCVTest/Constants.cs
+++ OpenCVTest/Constants.cs
@@ -7,5 +7,12 @@
         public const string ImageTongsMoved = @"Images\IMG_20151020_155516713.jpg";
         public const string ImageTongsGone = @"Images\IMG_20151020_155520579.jpg";
         public const string ImageSpoonsMoved = @"Images\IMG_20151020_155534249.jpg";
+        public const string Image-D2-1 = "IMG_20151021_155526134";
+        public const string Image-D2-2 = "IMG_20151021_155535777";
+        public const string Image-D2-3 = "IMG_20151021_155538772";
+        public const string Image-D2-4 = "IMG_20151021_155541710";
+        public const string Image-D2-5 = "IMG_20151021_155545368";
+        public const string Image-D2-6 = "IMG_20151021_155559044";
+        public const string Image-D2-7 = "IMG_20151021_155605719";
     }
 }
